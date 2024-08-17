99b8ba3b6124ce49518bb8d19cb25aacbfed9e3d
Andrey <pikachu_andrey@vk.com>
2024-08-17 23:33:44 +0900

    Keyboard: Split driver into PS/2 keyboard and keyboard buffer manager

---
+++ kernel/arch/i386/drv/keyboard_buffer.c
@@ -0,0 +1,50 @@
+#include <kernel/drv/keyboard_buffer.h>
+#include <stddef.h>
+
+static uint16_t keyboard_buffer[KEYBOARD_BUFFER_SIZE] = {0};
+static size_t keyboard_buffer_start = 0;
+static size_t keyboard_buffer_end = 0;
+
+// TODO: Move it into unicode.c or make it static and move it into unicode.h
+uint16_t codepoint_to_utf8_short(uint16_t code) {
+    if(code <= 0xff) {
+        return code & 0b01111111;
+    }
+
+    uint32_t low = (code & 0b111111) | 0b10000000;
+    uint32_t hi = ((code >> 6) & 0b11111) | 0b11000000;
+
+    //return (hi << 8) | low;
+    return (low << 8) | hi;
+}
+
+// Добавляем символ в буфер клавиатуры
+void keyboard_add_to_buffer(uint16_t c) {
+    size_t next = (keyboard_buffer_end + 1) % KEYBOARD_BUFFER_SIZE;
+    if (next != keyboard_buffer_start) { // Проверка на переполнение буфера
+        keyboard_buffer[keyboard_buffer_end] = c;
+        keyboard_buffer_end = next;
+    }
+}
+
+// Получаем символ из буфера клавиатуры
+uint16_t keyboard_get_from_buffer() {
+    if (keyboard_buffer_start == keyboard_buffer_end) {
+        return '\0'; // Если буфер пуст, возвращаем нулевой символ
+    } else {
+        uint16_t c = keyboard_buffer[keyboard_buffer_start];
+        keyboard_buffer_start = (keyboard_buffer_start + 1) % KEYBOARD_BUFFER_SIZE;
+        return c;
+    }
+}
+
+// Функция для получения символа из буфера
+uint16_t keyboard_get_char() {
+    uint16_t c;
+    // Ждем, пока буфер не будет пуст
+    while ((c = keyboard_get_from_buffer()) == 0) {
+        // Здесь может быть добавлено ожидание (idle), чтобы не нагружать процессор
+    }
+    return c;
+}
+
--- kernel/arch/i386/drv/ps2_keyboard.c
+++ kernel/arch/i386/drv/ps2_keyboard.c
@@ -1,12 +1,10 @@
 #include "kernel/sys/gui/shell.h"
 #include <kernel/kernel.h>
 #include <kernel/sys/isr.h>
-#include <kernel/drv/keyboard.h>
+#include <kernel/drv/ps2_keyboard.h>
 #include <kernel/drv/tty.h>
 #include <stdint.h>
 #include <kernel/drv/ps2.h>
-
-#define KEYBOARD_BUFFER_SIZE 256
 
 #define KEY_UP 0x48
 #define KEY_DOWN 0x50
@@ -16,43 +14,6 @@ bool keyboard_ru = false;
 static volatile uint8_t alt_flag = 0;
 static volatile uint8_t shift_flag = 0;
 static volatile uint8_t caps_lock_flag = 0;
-
-static uint16_t keyboard_buffer[KEYBOARD_BUFFER_SIZE] = {0};
-static size_t keyboard_buffer_start = 0;
-static size_t keyboard_buffer_end = 0;
-
-// TODO: Move it into unicode.c or make it static and move it into unicode.h
-uint16_t codepoint_to_utf8_short(uint16_t code) {
-    if(code <= 0xff) {
-        return code & 0b01111111;
-    }
-
-    uint32_t low = (code & 0b111111) | 0b10000000;
-    uint32_t hi = ((code >> 6) & 0b11111) | 0b11000000;
-
-    //return (hi << 8) | low;
-    return (low << 8) | hi;
-}
-
-// Добавляем символ в буфер клавиатуры
-void keyboard_add_to_buffer(uint16_t c) {
-    size_t next = (keyboard_buffer_end + 1) % KEYBOARD_BUFFER_SIZE;
-    if (next != keyboard_buffer_start) { // Проверка на переполнение буфера
-        keyboard_buffer[keyboard_buffer_end] = c;
-        keyboard_buffer_end = next;
-    }
-}
-
-// Получаем символ из буфера клавиатуры
-uint16_t keyboard_get_from_buffer() {
-    if (keyboard_buffer_start == keyboard_buffer_end) {
-        return '\0'; // Если буфер пуст, возвращаем нулевой символ
-    } else {
-        uint16_t c = keyboard_buffer[keyboard_buffer_start];
-        keyboard_buffer_start = (keyboard_buffer_start + 1) % KEYBOARD_BUFFER_SIZE;
-        return c;
-    }
-}
 
 void keyboard_handler() {
     uint8_t scancode = inb(0x60);
@@ -109,16 +70,6 @@ void keyboard_handler() {
     pic_eoi(KEYBOARD_IRQ); // Отправляем сигнал "конец прерывания"
 }
 
-
-// Функция для получения символа из буфера
-uint16_t keyboard_get_char() {
-    uint16_t c;
-    // Ждем, пока буфер не будет пуст
-    while ((c = keyboard_get_from_buffer()) == 0) {
-        // Здесь может быть добавлено ожидание (idle), чтобы не нагружать процессор
-    }
-    return c;
-}
 
 void keyboard_init() {
     uint8_t stat;

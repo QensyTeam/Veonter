8058cd92124f44facf60267f20bac690136fbde0
SayoriTeam | pimnik98 <nikita.piminoff@yandex.ru>
2024-08-19 19:20:03 +0300

    Running the OS via WSL (requires QEMU installed on Windows)

---
+++ wsl.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+bash iso.sh
+@-mkdir /mnt/c/SayoriDev/
+mv Veonter.iso /mnt/c/SayoriDev/Veonter.iso
+"/mnt/c/Program Files/qemu/qemu-system-i386.exe" -cdrom "C:\\SayoriDev\\Veonter.iso" -serial mon:stdio -m 128M -name "Veonter DEV WSL MODE" -d guest_errors -rtc base=localtime -netdev user,id=net1,net=192.168.222.0,dhcpstart=192.168.222.128 -device virtio-net-pci,netdev=net1,id=mydev1,mac=52:54:00:6a:40:f8 $(QEMU_FLAGS_WSL)
